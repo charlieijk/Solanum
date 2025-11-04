d93675de02a72c4090befdaef86cc8ad108e4934
charlieijk <charliewcullen@gmail.com>
2025-11-04 14:33:03 -0800

    Initial Commit

---
+++ Solanum/SolanumApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolanumApp.swift
+//  Solanum
+//
+//  Created by Charlie on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SolanumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
